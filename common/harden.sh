497d32e1837f0a084c2c87256585f496359b2ded
aziz-nasri <aziz.nasri.dev@gmail.com>
2026-08-01 12:05:38 +0000

    feat(common): add SSH hardening feature.

--- common/harden.sh
+++ common/harden.sh
@@ -12,9 +12,23 @@ pkg_install unattended-upgrades
 sudo dpkg-reconfigure -plow unattended-upgrades
 
 # Minimazing the attack surface.
+ # remove unused packages
 
+sudo apt autoremove --purge &> /dev/null
+sudo apt clean &> /dev/null
+log_info "removed unused packages."
 
 # SSH hardening.
+ # creating a backup to rollback if somthing goes wrong.
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak.$(date +%s) &> /dev/null
+ # disabling password authentication
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config &> /dev/null
+ # disabling root login
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config &> /dev/null
+ # changing to non-standard port
+sudo sed -i 's/^#\?Port 22.*/Port 2307/' /etc/ssh/sshd_config &> /dev/null
+
+sudo systemctl restart sshd &> /dev/null
 
 
 # Network hardening / firewall.
