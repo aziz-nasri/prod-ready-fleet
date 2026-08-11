c74204d4202ef013df420b9aec3648cfeab801da
aziz-nasri <aziz.nasri.dev@gmail.com>
2026-08-11 12:05:28 +0000

    add app server specific health check.

---
+++ roles/app/health-extra.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+source health.conf
+
+echo -e "Data tier reachability:\n"
+nc -zv $DATA_IP $DATA_PORT &> /dev/null
+if [[ $? -ep 0 ]]; then
+    echo "Database server is reachable through port ${DATA_PORT}"
+else
+    echo "ERROR: Database server is not reachable through port ${DATA_PORT}."
+fi
+nc -zv $DNS_IP $DNS_PORT &> /dev/null
+if [[ $? -ep 0 ]]; then
+    echo "DNS is reachable through port ${DNS_PORT}."
+else
+    echo "ERROR: DNS is not reachable through port ${DNS_PORT}."
+fi
+
+# Get values
+current=$(systemctl show -p MemoryCurrent --value "$SERVICE")
+max=$(systemctl show -p MemoryMax --value "$SERVICE")
+# Handle unlimited (infinity)
+if [[ "$max" == "infinity" || "$max" -eq 0 ]]; then
+    echo "No MemoryMax limit set"
+fi
+# Calculate percentage
+percent=$(( current * 100 / max ))
+echo "Memory usage: $percent% ($current / $max bytes)"
+if (( percent >= THRESHOLD )); then
+    echo "WARNING: $SERVICE is at ${percent}% of MemoryMax!"
+else
+    echo "${SERVICE} Memory usage is under the Threshold."
+fi
