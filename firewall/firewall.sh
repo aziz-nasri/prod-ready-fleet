b3200f3be9c43b707049134dff10399a58e1af3c
aziz-nasri <aziz.nasri.dev@gmail.com>
2026-08-12 08:39:04 +0000

    fix syntax errors in firewall.sh

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -9,7 +9,7 @@ trap trap_cleanup EXIT
 
 # Proxy server Firewall configurations
 proxy_fw(){
-nft add chain inet filter forward { type filter hook forward priority 0; policy drop; } > /dev/null
+nft add chain inet filter forward '{ type filter hook forward priority 0; policy drop; }' > /dev/null
 
 nft add rule inet filter input iifname "$NAT_IF" tcp dport { $HTTP_PORT, $HTTPS_PORT } accept > /dev/null
 nft add rule inet filter input iifname "$INT_IF" ip saddr $MGMT_NET tcp dport $SSH_PORT accept > /dev/null
@@ -18,7 +18,7 @@ nft add rule inet filter forward ct state established,related accept > /dev/null
 nft add rule inet filter forward iifname "$INT_IF" oifname "$NAT_IF" ip saddr $INT_NET tcp dport { $HTTP_PORT, $HTTPS_PORT } accept > /dev/null
 
 nft add table inet nat > /dev/null
-nft add chaine inet nat postrouting { type nat hook postrouting priority 100; } > /dev/null
+nft add chain inet nat postrouting '{ type nat hook postrouting priority 100; }' > /dev/null
 nft add rule inet nat postrouting oifname $NAT_IF masquerade > /dev/null
 
 sudo sysctl -w net.ipv4.ip_forward=1 > /dev/null
@@ -28,7 +28,7 @@ sudo nft list ruleset | sudo tee /etc/nftables.conf > /dev/null
 
 # Application server Firewall configurations
 app_fw(){
-nft add chain inet filter output { type filter hook output priority 0; policy drop; } > /dev/null
+nft add chain inet filter output '{ type filter hook output priority 0; policy drop; }' > /dev/null
 
 nft add rule inet filter input ip saddr $PROXY_IP tcp dport $APP_PORT accept > /dev/null
 nft add rule inet filter input tcp dport $APP_PORT drop > /dev/null
@@ -43,6 +43,7 @@ nft add rule inet filter output tcp dport { $HTTP_PORT, $HTTPS_PORT } accept > /
 sudo nft list ruleset | sudo tee /etc/nftables.conf > /dev/null
 }
 
+# Database + DNS server Firewall configurations
 db_fw(){
 nft add rule inet filter input ct state invalid drop > /dev/null
 nft add rule inet filter input ip saddr $MGMT_NET tcp dport $SSH_PORT accept > /dev/null
@@ -52,7 +53,7 @@ nft add rule inet filter input ip saddr $APP_TIER tcp dport $DNS_PORT accept > /
 nft add rule inet filter input ip saddr $PROXY_IP udp dport $DNS_PORT  accept
 nft add rule inet filter input ip saddr $PROXY_IP tcp dport $DNS_PORT  accept
 
-nft add chain inet filter output { type filter hook output priority 0; policy drop; } > /dev/null
+nft add chain inet filter output '{ type filter hook output priority 0; policy drop; }' > /dev/null
 nft add rule inet filter output oif lo accept > /dev/null
 nft add rule inet filter output ct state established,related accept > /dev/null
 sudo nft list ruleset | sudo tee /etc/nftables.conf > /dev/null
@@ -61,17 +62,15 @@ sudo nft list ruleset | sudo tee /etc/nftables.conf > /dev/null
 if [[ $(hostname | grep -i "proxy") ]]; then
     log_info "Adding firewall rules to the proxy server."
     proxy_fw
-    log_info "finised adding firewall rules."
 elif [[ $(hostname | grep -i "app") ]]; then
     log_info "Adding firewall rules to the Application server."
     app_fw
-    log_info "finised adding firewall rules."
 elif [[ $(hostname | grep -i "database") ]]; then
-    log_info "Adding firewall rules to the Application server."
+    log_info "Adding firewall rules to the database server."
     db_fw
-    log_info "finised adding firewall rules."
 else
     log_warn "no server detected. no firewall rules applied."
 fi
 
+log_info "finised adding firewall rules."
 sudo systemctl reload nftables 2>/dev/null
