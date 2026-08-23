35816630301d593360f9ef4e73048ae67edefcc0
aziz-nasri <aziz.nasri.dev@gmail.com>
2026-08-23 11:02:05 +0000

    add mgmt.conf template and fix install.sh

--- roles/management/install.sh
+++ roles/management/install.sh
@@ -9,8 +9,8 @@ trap trap_cleanup EXIT
 
 # Network configuration
 
-[[ -f /etc/netplan/01-netcfg.yaml ]] || die "Netplan file was not found"
-NETPLAN_FILE="/etc/netplan/01-netcfg.yaml"
+[[ -f "/etc/netplan/${NETCFG}" ]] || die "Netplan file was not found"
+NETPLAN_FILE="/etc/netplan/${NETCFG}"
 
 backup_file "$NETPLAN_FILE"
 
@@ -27,10 +27,10 @@ network:
         - $ADDRESSES
       routes:
         - to: $DMZ_SUB
-          via: $MNG_ADD
+          via: $MNG_IP
           scope: link
         - to: $INT_SUB
-          via: $MNG_ADD
+          via: $MNG_IP
           scope: link
 EOF
 chmod 600 "$NETPLAN_FILE" > /dev/null > /dev/null
@@ -45,29 +45,29 @@ log_info "closing Ports..."
 close_ports $TO_BE_ClOSED_PORTS
 
 # SSH set up
-ssh-keygen -t ed25519 -f ~/.ssh/lab-fleet -C "admin@lab-fleet" -N "" > /dev/null
+ssh-keygen -t ed25519 -f ~/.ssh/lab-fleet -C "admin@lab-fleet" > /dev/null
 
 [[ -f ~/.ssh/config ]] || die "~/.ssh/config file was not found"
 SSH_CONFIG="~/.ssh/config"
 
 sudo tee ~/.ssh/config << EOF
 Host proxy srv1
-    HostName 10.0.10.10
+    HostName $PROXY_IP
     User admin
     IdentityFile ~/.ssh/lab-fleet
-    BindAddress 10.0.0.2
+    BindAddress $MNG_IP
 
 Host app srv2
-    HostName 10.0.20.65
+    HostName $APP_IP
     User admin
     IdentityFile ~/.ssh/lab-fleet
-    BindAddress 10.0.0.2
+    BindAddress $MNG_IP
 
 Host db srv3
-    HostName 10.0.20.130
+    HostName $DB_IP
     User admin
     IdentityFile ~/.ssh/lab-fleet
-    BindAddress 10.0.0.2
+    BindAddress $MNG_IP
 EOF
 
 chmod 600 ~/.ssh/config ~/.ssh/lab-fleet > /dev/null
@@ -75,5 +75,4 @@ chmod 600 ~/.ssh/config ~/.ssh/lab-fleet > /dev/null
 # ssh hardening 
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config > /dev/null
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config > /dev/null
-sudo sed -i 's/^#\?Port 22.*/Port 2307/' /etc/ssh/sshd_config > /dev/null
 sudo systemctl restart sshd > /dev/null
